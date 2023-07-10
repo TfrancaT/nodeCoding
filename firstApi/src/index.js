a7b51a69fe31a2038cb89b0ecd792b505baf46bf
TfrancaT <talesfranca.97@gmail.com>
2023-07-09 23:38:52 -0300

    trabalhando com rotas

--- firstApi/src/index.js
+++ firstApi/src/index.js
@@ -1,8 +1,20 @@
 const http = require('http');
+const users = require('./mocks/users');
+
 const server = http.createServer((request, response) => {
+
+    console.log(`Request Method: ${request.method} | Endpoint: ${request.url}`);
+
+    if(request.url === '/users' && request.method === 'GET') {
+        response.writeHead(200, {'Content-Type': 'application/json' } );
+        response.end(JSON.stringify(users));
+    } else {
+        response.writeHead(404, {'Content-Type': 'text/html' } );
+        response.end(` Cannot ${request.method} ${request.url} `);
+    }
     
-    response.writeHead(200, {'Content-Type': 'text/html' } );
-    response.end('<h1>Ola, Mundo!</h1>');
+//    response.writeHead(200, {'Content-Type': 'text/html' } );
+//    response.end('<h1>Ola, Mundo!</h1>');
 
 });
 
---
+++ firstApi/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Rafael'
+    },
+    {
+        id: 2,
+        name: 'Berenice'
+    },
+    {
+        id: 2,
+        name: 'Tales'
+    },
+]
